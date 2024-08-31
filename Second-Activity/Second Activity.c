5ec30ec27a6ffa1443d0078036504013bcf48082
francesanne <francesanne.riconalla@gmail.com>
2024-08-31 13:39:48 +0800

    Initial Commit

---
+++ Second-Activity/Second Activity.c
@@ -0,0 +1,17 @@
+#include "Second Activity.h"
+
+int checkGreater(int num){
+
+}
+int checkDivisible(int num){
+
+}
+int checkPattern(int num){
+
+}
+int checkHexadecimal(char ch){
+
+}
+int checkCharacter(char ch){
+
+}
